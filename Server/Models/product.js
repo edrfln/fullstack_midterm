46d23beea4e9d06ef8e9dd5feaea50c8c3a90b99
Edinia Rosa Filiana <84521162+edrfln@users.noreply.github.com>
2023-07-27 20:59:52 +0700

    add models for product

---
+++ Server/Models/product.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  productID: {
+    type: String,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  video: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Video",
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
